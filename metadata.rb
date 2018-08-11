52b641b555633e67828cd3f3ef7ff3e38378777a
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-08-11 09:29:45 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures vault'
 long_description 'Installs/Configures vault'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
